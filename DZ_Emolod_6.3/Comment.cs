1b9bfbc7b2afb23ce0bc1806910deecd7cdd24c7
NikitaSmetanin <NikitaSmetanin1705@gmail.com>
2023-12-26 19:23:39 +0200

    Добавьте файлы проекта.

---
+++ DZ_Emolod_6.3/Comment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_Emolod_6._3
+{
+    
+    internal class Comment
+    {
+        Random random = new Random();
+        static int autoInc = 0;
+        private int id = 0;
+        private string text = "";
+        private User user = null;
+        private int like = 0;
+        private int dislike = 0;
+
+        public Comment(User user, string text)
+        {
+            this.text = text;
+            this.user = user;
+            //this.id = ++autoInc;
+        }
+
+        public int getId() { return id; }
+        public string getText() { return text; }
+        public User getUser() { return user; }
+        public int getLike() { return like;}
+        public int getDislike() {  return dislike;}
+        public void setLike(int like) {  this.like = like;}
+        public void setDislike(int dislike) {  this.dislike = dislike;}
+    }
+}
---
+++ DZ_Emolod_6.3/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_Emolod_6._3
+{
+    internal class User
+    {
+        static int autoInc = 0;
+        private int id = 0;
+        private string name = "";
+
+        public User(string name) {
+            this.name = name;
+            this.id = ++autoInc;
+        }
+        public int getId() { return id; }
+        public string getName() { return name; } 
+    }
+}
